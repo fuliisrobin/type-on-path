6c69e4c98e6fcd57ab94461ededb1a254eeb03f0
filiisrobin <44071928@qq.com>
2025-02-08 13:18:05 +0800

    Update readme and minor update to main ts

--- src/main.ts
+++ src/main.ts
@@ -10,14 +10,14 @@ export type TypeOnPath<ObjectType, Path extends string> = Path extends ""
     : Left extends ""
     ? TypeOnPath<ObjectType, Right>
     : TypeOnPath<TypeOnPath<ObjectType, Left>, Right>
-  : Path extends `${infer Left}[${infer key}]${infer Right}`
+  : Path extends `${infer Left}[${infer Key}]${infer Right}`
   ? Left extends keyof ObjectType
-    ? TypeOnPath<ObjectType[Left], `[${key}]${Right}`>
+    ? TypeOnPath<ObjectType[Left], `[${Key}]${Right}`>
     : Left extends ""
     ? ObjectType extends Array<infer U>
       ? TypeOnPath<U, Right>
-      : key extends keyof ObjectType
-      ? TypeOnPath<ObjectType[key], Right>
+      : Key extends keyof ObjectType
+      ? TypeOnPath<ObjectType[Key], Right>
       : unknown
     : unknown
   : never;
